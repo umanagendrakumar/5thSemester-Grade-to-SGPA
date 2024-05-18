1b415b541bc6ad7edb02fef063723732751240b0
umanagendrakumar <your.email@example.com>
2024-05-19 00:03:51 +0530

    first commit

---
+++ percentage.js
@@ -0,0 +1,19 @@
+function hello() {
+    const arr = [];
+    const val = document.querySelectorAll('input');
+    for (let v of val) {
+        arr.push((v.value));
+    }
+    let total = 0;
+    for(let val of arr) {
+        total = total+parseFloat(val);
+    }
+    const cgpa = total/5;
+    const percent = ( cgpa - 0.5 )*10;
+    document.querySelector('.last').classList.add('result');
+    document.querySelector('.result').innerText = percent;
+}
+par
+
+
+
