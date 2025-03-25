552069922f5332cbfd8b9e0182b008d3cd63627c
umanagendrakumar <your.email@example.com>
2025-03-25 23:17:04 +0530

    first commit

--- grade41.js
+++ grade41.js
@@ -46,7 +46,10 @@ function calculate(key) {
     // if() block is to reset all stylings.
     // when user grades mismatched with our real array.
     if ((!(arr.every(i => real.includes(i))))) {
-        alert(`${funAlert[funVal]}`);
+        alert(`${funAlert[funVal]} 
+            or 
+            Let me know,
+            inCase of any other Possibilities to add.`);
         document.querySelector('.sgpa')
             .style
             .display = "none";
@@ -62,7 +65,7 @@ function calculate(key) {
     // else() block is to calculate SGPA.
     else {
         // Below for() loop is to convert GRADE to GRADE POINTS.
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < 7; i++) {
             let grade = arr[i];
             switch (grade) {
                 case 'a+':
@@ -97,7 +100,7 @@ function calculate(key) {
 
         // Below for() loop is to calculate sum with corresponding CREDITS.
         let total = 0;
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < 7; i++) {
             let num = arr[i];
             switch (i) {
                 case 0:
@@ -105,24 +108,17 @@ function calculate(key) {
                 case 2:
                 case 3:
                 case 4:
+                case 6:
                     total += num * 3;
                     break;
                 case 5:
-                    total += num * 0;
-                    break;
-                case 6:
-                case 7:
-                case 9:
-                    total += num * 1.5;
-                    break;
-                case 8:
                     total += num * 2;
                     break;
             }
         }
 
         // SGPA is rounded to 2 DECIMAL DIGITS.
-        const sgpa = (Math.round((total / 21.5) * 100)) / 100;
+        const sgpa = (Math.round((total / 20) * 100)) / 100;
 
         // selecting span.sgpa
         // changing display:none to display:block.
@@ -146,7 +142,55 @@ function calculate(key) {
             .display = "none";
 
         // scroll to End of the page vertically.
-        window.scrollTo(0, (document.body.scrollHeight));        
-    
+        window.scrollTo(0, (document.body.scrollHeight));
+
+        //It will execute when greeting button is clicked.
+        if (key === "greet") {
+
+            // selecting div.content2
+            // changing display:none to display:block.
+            document.querySelector('.content2')
+                .style
+                .display = "block";
+
+            // selecting p.greet-para
+            const greetPara = document.querySelector('.greet-para')
+
+            // Asking user to enter name.
+            let name = prompt("Enter Your Name ");
+
+            // Below if() is to check whether the name is EMPTY or NULL.
+            // and looping till name is not EMPTY or NULL.
+            if (name === '' || name === null) {
+                do {
+                    name = prompt("You Can't Escape from Entering Your Name.");
+                } while (name === '' || name === null)
+            }
+
+            // Below if() is to check whether the name is exceeding length 10.
+            // and Looping till name length is in range.
+            if (name.length > 10) {
+                do {
+                    name = prompt("Max Length Allowed for Name is 10.");
+                } while (name.length > 10)
+            }
+
+            name = name.toUpperCase();
+
+            // Greeting users according to their result(SGPA).
+            if (sgpa >= 8.0) {
+                document.querySelector('.greet-para').style.backgroundColor = "green";
+                greetPara.innerText = `Hii "${name}", Congrats.`;
+            } else if (sgpa >= 7.5) {
+                document.querySelector('.greet-para').style.backgroundColor = "yellowgreen";
+                greetPara.innerText = `Hii "${name}", Congrats.`;
+            } else if (sgpa >= 7.0) {
+                document.querySelector('.greet-para').style.backgroundColor = "orange";
+                greetPara.innerText = `Hii "${name}", Congrats.`;
+            } else {
+                document.querySelector('.greet-para').style.backgroundColor = "red";
+                greetPara.innerText = `Hii "${name}", You Need Improvement.`;
+            }
+        }
     }
 }
